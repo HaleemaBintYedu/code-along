ecf606951ee3c8b357cb13f30be34574874a2e46
Haleema Bint Yedu <bintyeduhaleema@gmail.com>
2022-08-10 14:24:31 +0000

    props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import Header from "./components/Header";
-import { Button1, Button2, Button3 } from "./components/Button";
-import {Image} from "./components/Image";
+import Button from "./components/Button";
+import { Image } from "./components/Image";
+import Navbar from "./components/Navbar";
+// import NavItem from "./components/NavItem";
+
 
 
 function App() {
   return (
       <div>
+        <Navbar />
         <Header />
-        <Button1 />
-        <Button2 />
-        <Button3 />
+        <Button label="Add to Cart" />
+        <Button label="Read more..." />
+        <Button label="Register" />
+        <Button label="Login" />
         <Image />
       </div>
   )
 }
+
+// import { Button1, Button2, Button3 } from "./components/Button";
+
+
+// function App() {
+//   return (
+//       <div>
+//         <Header />
+//         <Button1 />
+//         <Button2 />
+//         <Button3 />
+//         <Image />
+//       </div>
+//   )
+// }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,19 @@
-export const Button1 = () => {
-    return <button>Add to Cart</button>
-};
+function Button({ label }) {
+    return <button className="border-2 border-orange-500 py-2 px-5 rounded-full text-orange-500 hover:bg-orange-500 hover:text-white">{label}</button>;
+}
 
-export const Button2 = () => {
-    return <button>Login</button>
-};
+export default Button;
 
-export const Button3 = () => {
-    return <button>Register</button>
-};
+
+
+// export const Button1 = () => {
+//     return <button className="bg-orange-500 py-2 px-5 text-white rounded-full">Add to Cart</button>
+// };
+
+// export const Button2 = () => {
+//     return <button className="bg-blue-500 p-2">Login</button>
+// };
+
+// export const Button3 = () => {
+//     return <button className="bg-green-500 p-2">Register</button>
+// };

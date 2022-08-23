867d6d89abc5204dfd103e8ca83eb50678f4af4c
Haleema Bint Yedu <bintyeduhaleema@gmail.com>
2022-08-23 16:19:31 +0000

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
-// import Header from "./components/Header";
-// import Button from "./components/Button";
-// import { Image } from "./components/Image";
-// import Navbar from "./components/Navbar";
-// import NavItem from "./components/NavItem";
-// import Practice from "./components/Practice";
-// import TaskManager from "./components/TaskManager";
-// import Form from "./components/Form";
-import ProductList from "./components/ProductList"
-// import Demo from "./components/Demo"
-// import demoNav from "./components/demoNav"
-import Spinner from "./components/Spinner";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import TaskManager from "./components/TaskManager";
+import ProductList from "./components/ProductList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      {/* <Navbar />  */}
-      {/* <Header /> */}
-      {/* <Button label="Add to Cart" /> */}
-      {/* <Button label="Read more..." /> */}
-      {/* <Button label="Register" /> */}
-      {/* <Button label="Login" /> */}
-      {/* <Image /> */}
-      {/* <Practice /> */}
-      {/* <TaskManager /> */}
-      {/* < Form /> */}
-      <ProductList />
-      {/* <Demo />
-        <demoNav /> */}
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/task-manager" element={<TaskManager />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-// import { Button1, Button2, Button3 } from "./components/Button";
-
-// function App() {
-//   return (
-//       <div>
-//         <Header />
-//         <Button1 />
-//         <Button2 />
-//         <Button3 />
-// <Image />
-//       </div>
-//   )
-// }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React from "react";
 import NavItem from "./NavItem";
 
+const links = [
+  {
+    label: "Home",
+    to: "/",
+  },
+  {
+    label: "Product",
+    to: "/products",
+  },
+  {
+    label: "Task Manager",
+    to: "/task-manager",
+  },
+  {
+    label: "About",
+    to: "/about",
+  },
+  {
+    label: "Contact",
+    to: "/contact",
+  },
+];
+
 function Navbar() {
-    return (
-        <nav className="flex justify-between py-3 px-20 shadow-md">
-            <h3 className="font-bold">Gob3Joint</h3>
-            <ul className="flex gap-5">
-                <NavItem menu="Home" />
-                <NavItem menu="Products" />
-                <NavItem menu="Services" />
-                <NavItem menu="About" />
-                <NavItem menu="Contact" />
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="flex justify-between py-3 px-20 shadow-md">
+      <h3 className="font-bold">Gob3 Joint</h3>
+      <ul className="flex gap-5">
+        {links.map((link, index) => (
+          <NavItem label={link.label} to={link.to} />
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
